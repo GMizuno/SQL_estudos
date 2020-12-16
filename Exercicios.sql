04b1824afd0ac63bb05ddc0bf2f4b9814e735cbc
GMizuno <gabrielmizuno@gmail.com>
2020-12-15 21:18:53 -0300

    Add alguns exercicios e terminei Join

---
+++ Exercicios.sql
@@ -0,0 +1,29 @@
+-- Ex: 01
+SELECT COUNT(*)
+FROM tb_products
+WHERE product_category_name = 'artes'
+
+-- Ex: 02
+-- Ex: 03
+-- Ex: 04
+-- Ex: 05
+-- Ex: 06
+-- Ex: 07
+-- Ex: 08
+-- Ex: 09
+-- Ex: 10
+-- Ex: 11
+-- Ex: 12
+-- Ex: 13
+-- Ex: 14
+
+-- Base 
+SELECT * FROM tb_products
+SELECT * FROM tb_geolocation
+SELECT * FROM tb_order_items
+SELECT * FROM tb_order_payments
+SELECT * FROM tb_order_reviews
+SELECT * FROM tb_orders
+SELECT * FROM tb_product_category_name_translation
+SELECT * FROM tb_products
+SELECT * FROM tb_sellers
